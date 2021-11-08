eb21219ed6b047747deb4f80a64456d5537549f0
Shoaib Rain <rainshoaib2013@outlook.com>
2021-11-07 18:46:14 -0600

    plot remaining

--- RAIN_1001825785_A8/problem2.py
+++ RAIN_1001825785_A8/problem2.py
@@ -1,3 +1,5 @@
+from scipy.spatial import distance
+import numpy as np
 
 # (a) Create a Python script that generates a plot of the L1 distance versus L2
 # distance. That is, the y-axis represents the output of each function and the
@@ -8,13 +10,26 @@ def _plot():
 
 # (b) Create a function in Python that, given two vectors, computes the L1 and L2
 # loss. The loss should be printed out.
-def _loss(L1,L2):
- pass
+def _loss(vectors):
+ v1,v2 = vectors
+ #calculate L1 loss from v1
+ L1 = round(distance.cityblock(v1,v2),2) #round to two decimal places
+ #calculate L2 loss from v2
+ L2 = round(distance.euclidean(v1,v2),2) #rounded to two decimal places
+ # the loss should be printed out
+ print(f'L1 loss is: {L1}')
+ print(f'L2 loss is: {L2}')
+ return (L1,L2)
 
 # (c) To test the above function, randomly generate two sets of scalars and pass
 # them as input to your function.
 def _random_scalar():
- pass
+ scalar1 =  np.random.randint(0, high=5, size=(1,3), dtype='l') #1x3 row vector
+ scalar12 =  np.random.randint(0, high=5, size=(1,3), dtype='l')#1x3 row vector
+ #pass these scalar into function to calculate loss L1 and L2
+ losses = _loss((scalar1,scalar12))
+ return losses
+
 
 if __name__ == '__main__':
- pass
+ _random_scalar()
--- RAIN_1001825785_A8/problem3.py
+++ RAIN_1001825785_A8/problem3.py
@@ -7,8 +7,10 @@ def _random_vector():
 
  return (rand_vec1, rand_vec2)
 
-def _projections():
+def _projections(vector):
+ v1, v2 = vector
  #calculate projection of two 3D vectors
+ projection = None
  #plot the result, sho wth original value on plot too
 
  #different color: show original vector as well as projected vector with different color
